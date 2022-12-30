5090a684cc67088de61cb2f6a3578ae977733eba
Andre Soares <andregsilv@gmail.com>
2022-12-30 01:33:03 -0300

    Working

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,10 @@
 
 import * as vscode from 'vscode';
-import { DebugSession, TerminatedEvent } from "@vscode/debugadapter";
+import { DebugSession, OutputEvent, TerminatedEvent } from "@vscode/debugadapter";
 import 'child_process';
 import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('--- SHELL Debug activated 4 ---');
 	vscode.debug.registerDebugAdapterDescriptorFactory("shell", {
 		createDebugAdapterDescriptor: (session): vscode.ProviderResult<vscode.DebugAdapterDescriptor> => {
 			const config = new SpawnConfig();
@@ -18,7 +17,6 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {
-	console.log('--- SHELL Debug deactivated 2 ---');
 }
 
 function getProperty(session: vscode.DebugSession, property: string, fallbackValue: any) {
@@ -33,29 +31,34 @@ class SpawnConfig {
 }
 
 class SpawnDebugger extends DebugSession {
+	private proc: ChildProcessWithoutNullStreams | null = null;
 	constructor(private config: SpawnConfig) {
 		super();
+
 	}
 	protected launchRequest(): void {
-		console.log('launchRequest');
-
-		const proc = spawn(
+		this.proc = spawn(
 			this.config.command,
 			this.config.args,
 		);
 
-		proc.stdout.on('data', (data) => {
-			console.log('stdout', Buffer.from(data).toString());
-			vscode.debug.activeDebugConsole.append(data);
+		this.proc.stdout.on('data', (data) => {
+			const dataStr = Buffer.from(data).toString();
+			this.sendEvent(new OutputEvent(dataStr, "stdout"));
 		});
-		proc.stderr.on('data', (data) => {
-			console.log('stderr', Buffer.from(data).toString());
-			vscode.debug.activeDebugConsole.append(data);
+		this.proc.stderr.on('data', (data) => {
+			const dataStr = Buffer.from(data).toString();
+			this.sendEvent(new OutputEvent(dataStr, "stderr"));
 		});
 
-		proc.on('close', () => {
-			console.log('close proc');
-			this.sendEvent(new TerminatedEvent())
+		this.proc.on('close', () => {
+			this.sendEvent(new TerminatedEvent());
 		})
+	}
+
+	protected terminateRequest(): void {
+		if (this.proc) {
+			this.proc.kill()
+		}
 	}
 }
